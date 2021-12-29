634dc5fa0060f95af6ebe6f1e9eb62479fe79c07
이두산 <sxfc675@gmail.com>
2021-12-29 16:46:32 +0900

    [21.12.29] actor => pawn 상속을 이용한 오브젝트 생성

---
+++ TestUnrealEngine/Source/TestUnrealEngine/MyPawn.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPawn.h"
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+    
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+
+    RootComponent = Mesh;
+
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> SM(TEXT("StaticMesh'/Game/StarterContent/Props/SM_Couch.SM_Couch'"));
+
+    if (SM.Succeeded()) {
+        Mesh->SetStaticMesh(SM.Object);
+    }
+
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
