33e503b546af70615f7be280c1c2ac770d3f8292
이두산 <sxfc675@gmail.com>
2022-01-09 18:03:23 +0900

    [22.01.09] blueprint 를 이용해 방향키 및 yaw 컨트롤 구현
    blueprint는 C++에 비해 정교한 작업을 할 수 없는 대신 unreal engine 에서의 편의성을 월등한 면이 있다
    하지만 이를 통해 프로젝트를 진행할 경우 다양한 문제에 봉착할 수 있기에 이는 프로토타입에만 사용하는 것이 좋을 것 같다.
    게다가 C++보다 성능이 좋지는 못하다.
    
    재밌는 점이 있다.
    BluePrint -> C++ 클래스를 상속 받아 새로운 클래스를 생성하는 것은 안된다. 하지만
    C++ -> BluePrint 클래스를 상속 받아 새로운 클래스를 생성하는 것은 가능하다 !!

--- TestUnrealEngine/Source/TestUnrealEngine/MyGameModeBase.cpp
+++ TestUnrealEngine/Source/TestUnrealEngine/MyGameModeBase.cpp
@@ -5,5 +5,12 @@
 #include "MyCharacter.h"
 
 AMyGameModeBase::AMyGameModeBase() {
-    DefaultPawnClass = AMyCharacter::StaticClass();
+//    DefaultPawnClass = AMyCharacter::StaticClass();
+    
+    static ConstructorHelpers::FClassFinder<ACharacter> BP_Char(TEXT("/Users/mango_mac/CPP_UnrealEngine_basic/TestUnrealEngine/Content/BluePrints/BP_MyCharacter.BP_MyCharacter_C"));
+    // 블루 프린트 경로의 경우에는 마지막 경로에 '_C'를 널어줘f야 한다.
+    
+    if (BP_Char.Succeeded()){
+        DefaultPawnClass = BP_Char.Class;
+    }
 }
