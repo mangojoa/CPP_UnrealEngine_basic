426ff8aed7702533a61e43acfb1381b50902c1b9
mangojoa <sxfc675@gmail.com>
2022-01-17 12:46:13 +0900

    [22.01.17] 애니메이션 적용하는 방법의 원리

--- TestUnrealEngine/Source/TestUnrealEngine/MyAnimInstance.cpp
+++ TestUnrealEngine/Source/TestUnrealEngine/MyAnimInstance.cpp
@@ -3,3 +3,14 @@
 
 #include "MyAnimInstance.h"
 
+void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	// 부모을 가리키는 Super
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	// pawn 에 접속하여 speed의 사이즈를 접근하는 방식 (정석)
+	auto pawn = TryGetPawnOwner();
+	if (IsValid(pawn)) {
+		Speed = pawn->GetVelocity().Size();
+	}
+}
