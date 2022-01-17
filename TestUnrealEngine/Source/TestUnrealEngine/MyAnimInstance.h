66f4bc2d2d6683ae9f3cabb8433fe003e5829219
mangojoa <sxfc675@gmail.com>
2022-01-17 11:11:58 +0900

    [22.01.17] 게임 애니메이션을 위한 C++ 클래스 추가

---
+++ TestUnrealEngine/Source/TestUnrealEngine/MyAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VehicleAnimInstance.h"
+#include "MyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTUNREALENGINE_API UMyAnimInstance : public UVehicleAnimInstance
+{
+	GENERATED_BODY()
+	
+};
