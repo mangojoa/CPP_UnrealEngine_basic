e3b91ec1c3259b0a08c8500bc72fcbe36a0e0edc
mangojoa <sxfc675@gmail.com>
2022-01-17 12:45:47 +0900

    [22.01.17] 애니메이션 적용하기

--- TestUnrealEngine/Source/TestUnrealEngine/MyAnimInstance.h
+++ TestUnrealEngine/Source/TestUnrealEngine/MyAnimInstance.h
@@ -6,12 +6,22 @@
 #include "VehicleAnimInstance.h"
 #include "MyAnimInstance.generated.h"
 
-/**
+/** [22.01.17] 캐릭터의 애니메이션을 넣기 위한 C++ 클래스 이다
  * 
  */
 UCLASS()
 class TESTUNREALENGINE_API UMyAnimInstance : public UVehicleAnimInstance
 {
 	GENERATED_BODY()
+
+		// 캐릭터의 움직임 즉 speed를 관리해주는 함수라고 보면된다.
+		// 이를 애님인스턴스에 선언하는 것이 일반적이다.
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 	
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Pawn, Meta=(AllowPrivateAccess=true))
+		float Speed;
 };
+
+// 
