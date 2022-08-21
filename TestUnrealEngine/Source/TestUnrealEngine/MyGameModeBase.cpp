6d52b8fe826a6a20454a806a38966436e59c56a7
mangojoa <sxfc675@gmail.com>
2022-08-21 22:45:28 +0900

    [22.08.21] 블루프린트 경로를 mac환경에서 window환경으로 변경

--- TestUnrealEngine/Source/TestUnrealEngine/MyGameModeBase.cpp
+++ TestUnrealEngine/Source/TestUnrealEngine/MyGameModeBase.cpp
@@ -7,7 +7,7 @@
 AMyGameModeBase::AMyGameModeBase() {
 //    DefaultPawnClass = AMyCharacter::StaticClass();
     
-    static ConstructorHelpers::FClassFinder<ACharacter> BP_Char(TEXT("/Users/mango_mac/CPP_UnrealEngine_basic/TestUnrealEngine/Content/BluePrints/BP_MyCharacter.BP_MyCharacter_C"));
+    static ConstructorHelpers::FClassFinder<ACharacter> BP_Char(TEXT("Blueprint'/Game/BluePrints/BP_MyCharacter.BP_MyCharacter_Char'"));
     // 블루 프린트 경로의 경우에는 마지막 경로에 '_Char'를 넣어줘야 한다.
     
     if (BP_Char.Succeeded()){
